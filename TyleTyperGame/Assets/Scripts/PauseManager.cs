fc290afacf68964d116795b45df771a0c4cb8f32
rosenbergk <ktros49@gmail.com>
2025-02-24 15:36:14 +0100

    Added pause menu

--- TyleTyperGame/Assets/Scripts/PauseManager.cs
+++ TyleTyperGame/Assets/Scripts/PauseManager.cs
@@ -1,9 +1,12 @@
 // PauseManager.cs
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseMenuUI;
+    
     private bool isPaused = false;
 
     void Update()
@@ -16,12 +19,26 @@ public class PauseManager : MonoBehaviour
 
     public void TogglePause()
     {
-        isPaused = !isPaused;
-        Time.timeScale = isPaused ? 0 : 1;
+        if (isPaused)
+            ResumeGame();
+        else
+            PauseGame();
+    }
 
-        // if (pauseMenuUI != null)
-        // {
-        //     pauseMenuUI.SetActive(isPaused);
-        // }
+    public void PauseGame() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void GoToMainMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
